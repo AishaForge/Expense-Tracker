3767b70de5fccdcbb789e199742c151176331f38
ayooshasaad75@gmail.com <ayooshasaad75@gmail.com>
2026-09-20 04:10:51 +0300

    Create main layout and sidebar

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ExpenseTracher
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlSidebar = new System.Windows.Forms.Panel();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.btnTransaction = new System.Windows.Forms.Button();
+            this.btnBudget = new System.Windows.Forms.Button();
+            this.btnSavings = new System.Windows.Forms.Button();
+            this.btnReports = new System.Windows.Forms.Button();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.lblDashTitle = new System.Windows.Forms.Label();
+            this.pnlSidebar.SuspendLayout();
+            this.pnlMain.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // pnlSidebar
+            // 
+            this.pnlSidebar.Controls.Add(this.btnReports);
+            this.pnlSidebar.Controls.Add(this.btnSavings);
+            this.pnlSidebar.Controls.Add(this.btnBudget);
+            this.pnlSidebar.Controls.Add(this.btnTransaction);
+            this.pnlSidebar.Controls.Add(this.btnDashboard);
+            this.pnlSidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlSidebar.Location = new System.Drawing.Point(0, 0);
+            this.pnlSidebar.Name = "pnlSidebar";
+            this.pnlSidebar.Size = new System.Drawing.Size(220, 944);
+            this.pnlSidebar.TabIndex = 0;
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.Location = new System.Drawing.Point(43, 77);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(133, 30);
+            this.btnDashboard.TabIndex = 0;
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.UseVisualStyleBackColor = true;
+            // 
+            // btnTransaction
+            // 
+            this.btnTransaction.Location = new System.Drawing.Point(43, 139);
+            this.btnTransaction.Name = "btnTransaction";
+            this.btnTransaction.Size = new System.Drawing.Size(133, 27);
+            this.btnTransaction.TabIndex = 1;
+            this.btnTransaction.Text = "Transactions";
+            this.btnTransaction.UseVisualStyleBackColor = true;
+            // 
+            // btnBudget
+            // 
+            this.btnBudget.Location = new System.Drawing.Point(43, 207);
+            this.btnBudget.Name = "btnBudget";
+            this.btnBudget.Size = new System.Drawing.Size(133, 29);
+            this.btnBudget.TabIndex = 2;
+            this.btnBudget.Text = "Budget";
+            this.btnBudget.UseVisualStyleBackColor = true;
+            // 
+            // btnSavings
+            // 
+            this.btnSavings.Location = new System.Drawing.Point(43, 275);
+            this.btnSavings.Name = "btnSavings";
+            this.btnSavings.Size = new System.Drawing.Size(133, 29);
+            this.btnSavings.TabIndex = 3;
+            this.btnSavings.Text = "Saving Goals";
+            this.btnSavings.UseVisualStyleBackColor = true;
+            // 
+            // btnReports
+            // 
+            this.btnReports.Location = new System.Drawing.Point(43, 340);
+            this.btnReports.Name = "btnReports";
+            this.btnReports.Size = new System.Drawing.Size(133, 29);
+            this.btnReports.TabIndex = 4;
+            this.btnReports.Text = "Reports";
+            this.btnReports.UseVisualStyleBackColor = true;
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Controls.Add(this.lblDashTitle);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(220, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(458, 944);
+            this.pnlMain.TabIndex = 1;
+            // 
+            // lblDashTitle
+            // 
+            this.lblDashTitle.AutoSize = true;
+            this.lblDashTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDashTitle.Location = new System.Drawing.Point(56, 41);
+            this.lblDashTitle.Name = "lblDashTitle";
+            this.lblDashTitle.Size = new System.Drawing.Size(224, 46);
+            this.lblDashTitle.TabIndex = 5;
+            this.lblDashTitle.Text = "Dashboard";
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(992, 671);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(678, 944);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.pnlSidebar);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Expense Tracker";
+            this.pnlSidebar.ResumeLayout(false);
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlSidebar;
+        private System.Windows.Forms.Button btnReports;
+        private System.Windows.Forms.Button btnSavings;
+        private System.Windows.Forms.Button btnBudget;
+        private System.Windows.Forms.Button btnTransaction;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Label lblDashTitle;
     }
 }
 
